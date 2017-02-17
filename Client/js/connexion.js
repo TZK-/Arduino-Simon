21bacf4caa691d62c187371a33dc449735ac03f9
Gwenael_Cruz <cruz.gwenael@gmail.com>
2017-02-17 15:30:18 +0100

    Quelques ajouts mineurs

--- Client/js/connexion.js
+++ Client/js/connexion.js
@@ -3,12 +3,12 @@ const WebSocket = require('ws');
 const ws = new WebSocket('ws://www.host.com/path');
 
 ws.on('open', function open() {
-    console.log('Récupération des informations du serveur')
+    console.log('Ouverture de la communication avec le serveur');
     ws.send('OK');
 });
 
 
 ws.on('message', function(data, flags){
-    // flags.binary will be set if a binary data is received.
-    // flags.masked will be set if the data was masked.
+    console.log("Récupération d'un message du serveur");
+
 });
