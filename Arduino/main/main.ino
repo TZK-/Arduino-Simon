fe220f19f8c4db5605681446c3098ec55d7f81cf
Yohan Garde <yohan.garde@iut-valence.fr>
2017-03-03 16:33:29 +0100

    Suppression de la fonction frequency() (elle n'est pas utilisée car le son du buzzer ne change presque pas avec la fréquence)

--- Arduino/main/main.ino
+++ Arduino/main/main.ino
@@ -198,36 +198,3 @@ void sendSequence(node_t* head) {
   }
   Serial.println("]}");
 }
-
-int frequency(char note) 
-{
-  // This function takes a note character (a-g), and returns the
-  // corresponding frequency in Hz for the tone() function.
-
-  int i;
-  const int numNotes = 8;  // number of notes we're storing
-
-  // The following arrays hold the note characters and their
-  // corresponding frequencies. The last "C" note is uppercase
-  // to separate it from the first lowercase "c". If you want to
-  // add more notes, you'll need to use unique characters.
-
-  // For the "char" (character) type, we put single characters
-  // in single quotes.
-
-  char names[] = { 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'C' };
-  int frequencies[] = {262, 294, 330, 349, 392, 440, 494, 523};
-
-  // Now we'll search through the letters in the array, and if
-  // we find it, we'll return the frequency for that note.
-
-  for (i = 0; i < numNotes; i++)  // Step through the notes
-  {
-    if (names[i] == note)         // Is this the one?
-    {
-      return(frequencies[i]);     // Yes! Return the frequency
-    }
-  }
-  return(0);  // We looked through everything and didn't find it,
-              // but we still need to return a value, so return 0.
-}
