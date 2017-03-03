b554d0e24f0b500fdf823ca42decb518d4ce7364
TZK- <thib-17@orange.fr>
2017-03-03 12:44:44 +0100

    Add server ws

--- Server/index.js
+++ Server/index.js
@@ -1,12 +1,15 @@
 var SerialPort = require('serialport');
+const WebSocket = require('ws');
 var express = require('express');
 var app = express();
 
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+	res.send('Hello World!');
 });
 
-var port = new SerialPort('COM3');
+
+/*var port = new SerialPort('COM3');
 
 port.on("open", function () {
 	console.log('open');
@@ -17,4 +20,17 @@ port.on("open", function () {
 		});
 		console.log(jsonText);
 	});
+});*/
+
+app.listen(3000);
+
+const wss = new WebSocket.Server({ port: 8080 });
+console.log("OK WS");
+wss.on('connection', function connection(ws) {
+	console.log("Connected..");
+	
+	ws.send("SALUT");
+
 });
+
+
