10285ff889a8d877f7f202df41effa244cb42b75
Gwenael_Cruz <cruz.gwenael@gmail.com>
2017-03-03 16:24:48 +0100

    Changements sur l'envoi de messages sur le websocket

--- Client/js/connection.js
+++ Client/js/connection.js
@@ -1,21 +1,10 @@
 
-var ws = new WebSocket("ws://172.26.123.13:8080");
+    let ws = new WebSocket("ws://172.26.123.13:8080");
+    ws.onmessage = function(data, flags){
+        //console.log("Récupération d'un message du serveur");
+        console.log(data.data);
+    };
 
-
-
-ws.onmessage = function(data, flags){
-    //console.log("Récupération d'un message du serveur");
-    console.log(data.data);
-
-};
-
-var expected = ['red', 'green', 'blue'];
-var res = [];
-var failed = false;
-var over = false;
-
-$('.simon').click(function() {
-
-    ws.send($(this).attr('id'));
-
-});
+    let socket = () => {
+        return ws;
+    }
